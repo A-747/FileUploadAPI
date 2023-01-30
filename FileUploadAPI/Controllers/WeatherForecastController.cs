facb84468606ef035ae1ed58860453de09cf8d4e
Alolika <Alolika@ALOLIKA>
2023-01-30 20:00:10 +0530

    added the piece of code for creating memory / RAM / heap usage spike and cpu usage spike

--- FileUploadAPI/Controllers/WeatherForecastController.cs
+++ FileUploadAPI/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,10 +23,18 @@ namespace FileUploadAPI.Controllers
         {
             _logger = logger;
         }
-
+        public static ArrayList abc = new ArrayList();
+        public static ArrayList cde = new ArrayList();
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            abc.Add(this);
+            cde.Add(this);
+            int i = 0;
+            while(i>=0)
+            {
+                i++;
+            }
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
